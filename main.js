4b8627b916f766de48191d02d821f50b118c19e8
Leon Skinner <Leon.skinner@bt.com>
2024-01-29 14:15:14 +0000

    add negative time

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const min_pointer = document.getElementById('pointer');
   const min_dial = document.getElementById('minuet_circle');
   const rotateButton = document.getElementById('rotateButton');
-  
+  const negative_h_rotate = document.getElementById('negative_h_rotate');
+  const negative_min_rotate = document.getElementById('negative_min_rotate');
   
   min_rotate.addEventListener('click', function () {
     rotation -= 7.5;
@@ -23,6 +24,16 @@ document.addEventListener('DOMContentLoaded', function () {
     min_rotation += 6;
     day_circle.style.transform = `rotate(${rotation}deg)`;
     min_pointer.style.transform = `rotate(${min_rotation}deg)`;
+  });
+
+  negative_h_rotate.addEventListener('click', function () {
+    rotation += 15;
+    day_circle.style.transform = `rotate(${rotation}deg)`;
+  });
+
+  negative_min_rotate.addEventListener('click', function () {
+    rotation += 7.5;
+    day_circle.style.transform = `rotate(${rotation}deg)`;
   });
   
 });
